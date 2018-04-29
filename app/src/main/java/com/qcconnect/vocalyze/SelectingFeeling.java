f5131eab25681c1df6683cd2636c0336b40b50c9
Bernice Tran <bernicetran94@gmail.com>
2018-04-29 12:16:48 -0400

    Edit emotion list

--- app/src/main/java/com/qcconnect/vocalyze/SelectingFeeling.java
+++ app/src/main/java/com/qcconnect/vocalyze/SelectingFeeling.java
@@ -1,14 +1,13 @@
 package com.qcconnect.vocalyze;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.ListAdapter;
-
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 public class SelectingFeeling extends AppCompatActivity {
@@ -19,28 +18,43 @@ public class SelectingFeeling extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selecting_feeling);
 
-        String[] emotions = {"ANGRY", "BORED", "CONFLICTED", "CONFUSED", "CURIOUS", "DISGUSTED", "EMBARRASSED", "HEARTBROKEN", "LIFELESS", "LONELY", "LOST", "SAD", "SCARED", "SHOCKED", "TIRED"};
-        ListAdapter emotionAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, emotions);
-        ListView emotionListView =  findViewById(R.id.emotion_list);
-        emotionListView.setAdapter(emotionAdapter);
-
-
-        emotionListView.setOnItemClickListener(
-                new AdapterView.OnItemClickListener() {
-                    @Override
-                    public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                        String emotion = String.valueOf(adapterView.getItemAtPosition(position));
-
-                        //
-                        // add emotion to profile
-
-
-                    }
-                }
-        );
-
+        RecyclerView emotionListView =  findViewById(R.id.emotion_list);
+        emotionListView.setAdapter(new EmotionAdapter());
+        emotionListView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
 
     }
 
 
+    public class EmotionAdapter extends RecyclerView.Adapter<EmotionAdapter.EmotionViewHolder> {
+        String[] emotions = {"ANGRY", "BORED", "CONFLICTED", "CONFUSED", "CURIOUS", "DISGUSTED", "EMBARRASSED", "HAPPY", "HEARTBROKEN", "LIFELESS", "LONELY", "LOST", "SAD", "SCARED", "SHOCKED", "TIRED"};
+
+        @Override
+        public EmotionViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(getApplicationContext()).inflate(R.layout.emotion_list_item, parent, false);
+            return new EmotionViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(EmotionViewHolder holder, int position) {
+            holder.textView.setText(emotions[position]);
+        }
+
+        @Override
+        public int getItemCount() {
+            return emotions.length;
+        }
+
+
+        class EmotionViewHolder extends RecyclerView.ViewHolder {
+            TextView textView;
+
+            public EmotionViewHolder(View itemView) {
+                super(itemView);
+                textView = itemView.findViewById(R.id.emotion_listitem);
+            }
+        }
+//    }
+
+
+    }
 }
